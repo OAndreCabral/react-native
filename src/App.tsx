5a87eba78f9dd7d46c57b343d4a47688ab576ddc
Andre.Cabral <andre.cabral@db1.com.br>
2024-04-04 14:19:13 -0300

    enviroment

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Texto</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
